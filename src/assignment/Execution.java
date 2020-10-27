1624190be5dfb1ec128cf9ecc4cb15bdebd686c3
Mansi katiyar <mansi.katiyar_cs19@gla.ac.in>
2020-10-27 12:10:23 +0530

    create the variable of enum type

--- src/assignment/Execution.java
+++ src/assignment/Execution.java
@@ -1,5 +1,10 @@
 package assignment;
 
 public class Execution {
+    public static void main(String[] args) {
+        Months months;
+        months = Months.JANUARY;
+        System.out.println(months.name());
+    }
 
 }
