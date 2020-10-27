e2fc2eec9b961a8a4efdb37ac2163fd734fe9894
Mansi katiyar <mansi.katiyar_cs19@gla.ac.in>
2020-10-27 12:07:39 +0530

    create the execution class

---
+++ src/assignment/Months.java
@@ -0,0 +1,17 @@
+package assignment;
+
+public enum Months {
+    JANUARY, //(totalNumberOfDays:31),
+    FEBRUARY, //(totalNumberOfDays:)
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER;
+
+}
